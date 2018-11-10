f320d64b68cf478943f492e50856564c2fb473d1
junyuliu1994 <junyuliu@email.arizona.edu>
2018-11-09 21:38:26 -0700

    jl

--- src/MarioGameView.java
+++ src/MarioGameView.java
@@ -1,90 +1,64 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Observable;
-import java.util.Observer;
-
 import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class MarioGameView extends Application implements Observer{
-	
-	
-	Image i = new Image("file:marioRight2Lvl0.png", false);
+public class MarioGameView extends Application {
 
-	
-	public static void main(String[] args) {
+    private static final Image IMAGE = new Image("file:320px-The_Horse_in_Motion.jpg");
+    private static final int COLUMNS  =   4;
+    private static final int COUNT    =  10;
+    private static final int OFFSET_X =  10;
+    private static final int OFFSET_Y =  6;
+    private static final int WIDTH    = 75;
+    private static final int HEIGHT   = 50;
+
+    public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage stage) { 
-    	Canvas canvas = new Canvas(1280, 720);
-    	GraphicsContext gc = canvas.getGraphicsContext2D();
-    	gc.drawImage(i, 640, 360);
-    	
-    	Pane pane = new Pane();
-    	pane.getChildren().add(canvas);
-        Scene scene = new Scene(pane);
-        scene.setOnKeyPressed(event -> {
-        	 if (event.getCode() == KeyCode.RIGHT) {
-        		 new AnimationTimer() {
-        			 public void handle(long currentNanoTime) {
-		        		i = new Image("file:marioRight3Lvl0.png", false);
-		        		gc.clearRect(0, 0, 1280, 720); 
-		        	    gc.drawImage(i, 640, 360);
-		        	            	    
-		        	    i = new Image("file:marioRight0Lvl0.png", false);
-		        		gc.clearRect(0, 0, 1280, 720); 
-		        	    gc.drawImage(i, 640, 360);
-		        	    
-		        	    i = new Image("file:marioRight1Lvl0.png", false);
-		        		gc.clearRect(0, 0, 1280, 720); 
-		        	    gc.drawImage(i, 640, 360);
-		        	    System.out.println("QWE");
-        			 }
-        		 }.start();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("The Horse in Motion");
+        
+        Canvas canvas = new Canvas(1280, 720);
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        
 
-        	 } else if (event.getCode() == KeyCode.LEFT) {
+        final ImageView imageView = new ImageView(IMAGE);
+        gc.drawImage(imageView.getImage(), // the image to be drawn or null.
+        		OFFSET_X, // the source rectangle's X coordinate position.
+        		OFFSET_Y, // the source rectangle's Y coordinate position.
+        		WIDTH, // the source rectangle's width.
+        		HEIGHT, // the source rectangle's height.
+        		0, // the destination rectangle's X coordinate position.
+        		0, // the destination rectangle's Y coordinate position.
+        		WIDTH, // the destination rectangle's width.
+        		HEIGHT); // the destination rectangle's height. 
+        
+        Group root = new Group();
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        
 
-        	 }
-        });
+        final Animation animation = new SpriteAnimation(
+                imageView,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT, gc
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
         
-        
-        scene.setOnKeyReleased(event -> {
-        	i = new Image("file:marioRight2Lvl0.png", false);
-    		gc.clearRect(0, 0, 1280, 720); 
-    	    gc.drawImage(i, 640, 360);
-        });
-        stage.setScene(scene);
-        stage.setTitle("Mario Game");
-        
-        AnimationTimer timer = new AnimationTimer() {
-        	public void handle(long now) {
-        		
-        	}
-        };
-        timer.start();
-     
-        stage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
