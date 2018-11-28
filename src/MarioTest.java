231ee8c06e70b596a3baa69088f404b60368316f
WatcherOfTheWorld <code_m@outlook.com>
2018-11-27 19:37:14 -0700

    change the implantation

--- src/MarioTest.java
+++ src/MarioTest.java
@@ -1,14 +1,35 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 
 public class MarioTest {
     @Test
     void ControllerTest(){
-        TestThread testThread = new TestThread();
-        testThread.run();
-        while(testThread.controller != null) {
-        }
-        System.out.print("init complete");
+        GameModel model = new GameModel();
+        GameController controller = new GameController(model);
     }
+
+
+    public static class SimpleFX extends Application {
+        public void start(Stage primaryStage) {
+        }
+    }
+
+    @BeforeAll
+    public static void initSimpleFX() throws InterruptedException{
+        Thread t = new Thread("new"){
+            @Override
+            public void run() {
+                Application.launch(SimpleFX.class, new String[0]);
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(100);
+    }
+
 }
