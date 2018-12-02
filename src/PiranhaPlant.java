6e5bd96cb7c56c2349791a433e19a264ef7fdc5f
a502583819 <pengyuyang@email.arizona.edu>
2018-12-02 15:36:22 -0700

    flower

--- src/PiranhaPlant.java
+++ src/PiranhaPlant.java
@@ -3,6 +3,7 @@ import javafx.scene.image.Image;
 public class PiranhaPlant extends Monster{
 	private static Image image = new Image("resources/enemies2.png");
 	
+	
 	public PiranhaPlant(int offset_x, int offset_y,int x, int y) {
 		super(image, offset_x, offset_y, 40, 80, x, y);
 		setInitiall_offsetX(480);
